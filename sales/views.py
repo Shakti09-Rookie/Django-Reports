f0763c5b1a076f434220a6a0d8efa850524a8092
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-09-13 23:55:00 +0530

    search form and get form data

--- sales/views.py
+++ sales/views.py
@@ -1,11 +1,22 @@
+import re
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 # Create your views here.
 
 def home_view(request):
-    hello = 'hello world from view'
-    return render(request, 'sales/home.html', {'hello' : hello})
+    form = SalesSearchForm(request.POST or None)
+
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+
+    context = {
+        'form' : form,
+    }
+    return render(request, 'sales/home.html', context)
 
 class SaleListView(ListView):
     model = Sale
