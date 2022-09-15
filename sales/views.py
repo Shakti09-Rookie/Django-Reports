ec9e19ac2e378669c1bf23fd35e44dcf15d20f54
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-09-15 22:14:25 +0530

    dataframe in template

--- sales/views.py
+++ sales/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 # Create your views here.
 
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -13,8 +15,22 @@ def home_view(request):
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
 
+        # lte = less than equal to, gte = greater than equal to
+        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        # qs2 = Sale.objects.get(id=1)
+        if len(qs) > 0:        
+            print(qs)
+            sales_df = pd.DataFrame(qs.values())
+            sales_df = sales_df.to_html()
+            
+            # df2 = pd.DataFrame(qs1.values_list())
+            print(sales_df)
+        else:
+            print("No data")
+
     context = {
         'form' : form,
+        'sales_df' : sales_df,
     }
     return render(request, 'sales/home.html', context)
 
