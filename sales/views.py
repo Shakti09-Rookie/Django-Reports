ca2737152ff1a8ad17f589d5a0a76c268ffd5267
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-09-16 15:58:47 +0530

    dataframe showing ids

--- sales/views.py
+++ sales/views.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 def home_view(request):
     sales_df = None
+    positions_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -16,21 +17,38 @@ def home_view(request):
         chart_type = request.POST.get('chart_type')
 
         # lte = less than equal to, gte = greater than equal to
-        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
-        # qs2 = Sale.objects.get(id=1)
-        if len(qs) > 0:        
-            print(qs)
-            sales_df = pd.DataFrame(qs.values())
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        # sale_qs2 = Sale.objects.get(id=1)
+        if len(sale_qs) > 0:        
+            print(sale_qs)
+            sales_df = pd.DataFrame(sale_qs.values())
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'product_id' : pos.id,
+                        'product' : pos.product.name,
+                        'quantity' : pos.quantity,
+                        'price' : pos.price,
+                        'sales_id' : pos.get_sales_id(),
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data)
+            print("positions_df")
+            print(positions_df)
+
+
             sales_df = sales_df.to_html()
-            
-            # df2 = pd.DataFrame(qs1.values_list())
-            print(sales_df)
+            positions_df = positions_df.to_html()
+            # df2 = pd.DataFrame(sale_qs1.values_list())
         else:
             print("No data")
 
     context = {
         'form' : form,
         'sales_df' : sales_df,
+        'positions_df' : positions_df,
     }
     return render(request, 'sales/home.html', context)
 
