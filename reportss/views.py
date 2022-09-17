08cfacebab2918612efac73d5fa81553fe7aa346
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-09-17 20:59:07 +0530

    Rpoerts done

--- reportss/views.py
+++ reportss/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from profiles.models import Profile
+from django.http import JsonResponse
+from .utils import get_report_image
+from .models import Report
+from .forms import ReportForm
 # Create your views here.
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+def create_report_view(request):
+    form = ReportForm(request.POST or None)
+    if is_ajax(request=request):
+        # name = request.POST.get('name')
+        # remarks = request.POST.get('remarks')
+
+        image = request.POST.get('image')
+        img = get_report_image(image)
+        author = Profile.objects.get(user=request.user)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.image = img
+            instance.author = author
+            instance.save()
+
+        return JsonResponse({'msg' : 'send'})
+    return JsonResponse({})
+
+# def create_report_view(request):
+#     if is_ajax(request=request):
+#         name = request.POST.get('name')
+#         remarks = request.POST.get('remarks')
+#         image = request.POST.get('image')
+
+#         img = get_report_image(image)
+
+#         author = Profile.objects.get(user=request.user)
+#         Report.objects.create(name=name, remarks=remarks, image=img, author=author)
+#         return JsonResponse({'msg' : 'send'})
+#     return JsonResponse({})
